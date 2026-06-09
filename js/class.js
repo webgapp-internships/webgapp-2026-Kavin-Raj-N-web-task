3aa1f840ae0a8b3713743d453b09007530451ce3
kavinrajn04 <kavinrajn0402@gmail.com>
2026-06-09 19:21:15 +0530

    Commiting...

---
+++ js/class.js
@@ -0,0 +1,12 @@
+class Student {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    ages() {
+        return `${this.name} age is ${this.age}`;
+    }
+}
+
+
