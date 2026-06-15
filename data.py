444c5615129dccfb5cfe80c2392a3f3e5e651c4c
kavinrajn04 <kavinrajn0402@gmail.com>
2026-06-15 12:12:53 +0530

    db commit

---
+++ data.py
@@ -0,0 +1,53 @@
+import psycopg2
+
+# conn = psycopg2.connect(
+#     host="localhost",
+#     database="postgres",
+#     user="postgres",
+#     password="kavin0402",
+#     port="5432",
+# )
+
+# conn.autocommit = True
+
+# cursor = conn.cursor()
+
+# cursor.execute("CREATE DATABASE student_db")
+
+# cursor.close()
+
+# conn.close()
+
+# print("Database Created")
+
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="student_db",
+    user="postgres",
+    password="kavin0402",
+    port="5432",
+)
+
+cursor = conn.cursor()
+
+# cursor.execute(
+#     "CREATE TABLE users(id SERIAL PRIMARY KEY, name VARCHAR(40), email VARCHAR(40))"
+# )
+
+# cursor.execute(
+#     "INSERT INTO users(name, email) VALUES(%s,%s)", ("Kavin", "kavinrajn0402@gmail.com")
+# )
+
+
+# cursor.execute("DELETE FROM users WHERE id = 2")
+# conn.commit()
+cursor.execute("SELECT * FROM users")
+
+
+for row in cursor.fetchall():
+    print(row)
+
+cursor.close()
+
+conn.close()
---
+++ html/polymorphism.py
@@ -0,0 +1,9 @@
+class Shape:
+    def area():
+        return 0
+
+    def perimeter():
+        return 0
+    
+    def describe():
+        f''
